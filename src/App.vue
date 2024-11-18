21fa7ffa7ea922b07d7de5b381f047b9017fab1b
yochatapa <yochatapa@gmail.com>
2024-11-18 23:04:44 +0900

    chore : 초기 세팅

---
+++ src/App.vue
@@ -0,0 +1,107 @@
+<script setup lang="ts">
+import { onMounted, onBeforeUnmount } from 'vue';
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+
+function handleScroll() {
+  console.log('User is scrolling!');
+}
+  
+onMounted(() => {
+  window.addEventListener('click', handleScroll);
+});
+</script>
+
+<template>
+  <header>
+    <nav>
+      <RouterLink to="/">About Me</RouterLink>
+      <RouterLink to="/career">Career</RouterLink>
+      <RouterLink to="/project">Projects</RouterLink>
+      <RouterLink to="/skills">Skills</RouterLink>
+      <RouterLink to="/contact">Contact</RouterLink>
+    </nav>
+
+    <!-- <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div> -->
+  </header>
+  <main>
+    main
+  </main>
+  <footer>
+  </footer>
+  <!-- <RouterView /> -->
+</template>
+
+<style scoped>
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+/*header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+
+
+
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}*/
+</style>
