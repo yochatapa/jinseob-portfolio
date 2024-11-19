23097606f099fdce74ebd84e46f403e74b606204
yochatapa <yochatapa@gmail.com>
2024-11-19 22:04:27 +0900

    feat: Card init
    카드 컴포넌트 init

--- src/views/about/AboutMe.vue
+++ src/views/about/AboutMe.vue
@@ -1,11 +1,18 @@
 <script setup lang="ts">
-    import NavButton from '@/components/NavButton.vue'
+import CardContainer from '@/components/card/CardContainer.vue';
+import CardDetail from '@/components/card/CardDetail.vue';
+import ProfileCard from '@/components/card/ProfileCard.vue';
+import NavButton from '@/components/NavButton.vue'
 </script>
 
 <template>
-    About Me
-    
-    <NavButton></NavButton>
+    <CardContainer>
+        <ProfileCard></ProfileCard>
+        <CardDetail></CardDetail>
+    </CardContainer>
+    <NavButton>
+        <div class="hihi"></div>
+    </NavButton>
 </template>
 
 <style scoped>
