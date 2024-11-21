4503627247d21ac5af52895ff0778bfeb7e67b82
yochatapa <yochatapa@gmail.com>
2024-11-21 19:55:46 +0900

    build: commonJS 추가

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,14 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import commonjs from 'vite-plugin-commonjs';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    commonjs()
   ],
   resolve: {
     alias: {
