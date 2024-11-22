e4e859e5e16f09104f8890e9b67f8ae2187970a8
yochatapa <yochatapa@gmail.com>
2024-11-22 21:22:57 +0900

    fix: index import 변경

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,10 @@
 import { createRouter, createWebHistory, createWebHashHistory } from 'vue-router'
 
-import AboutMe from '@/views/about/index.vue'
-import Career from '@/views/career/index.vue'
-import Projects from '@/views/projects/index.vue'
-import Skills from '@/views/skills/index.vue'
-import Contact from '@/views/contact/index.vue'
+import AboutMe from '@/views/about/AboutMe.vue'
+import Career from '@/views/career/Career.vue'
+import Projects from '@/views/projects/Projects.vue'
+import Skills from '@/views/skills/Skills.vue'
+import Contact from '@/views/contact/Contact.vue'
 
 
 const router = createRouter({
