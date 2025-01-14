7fd95e8e56554e5f2271147dbb8ed5b6e5a4a086
yochatapa <yochatapa@gmail.com>
2025-01-14 23:14:31 +0900

    feature: NameCard - 이미지 로딩 시, 로딩 서클 추가

--- src/components/card/NameCard.vue
+++ src/components/card/NameCard.vue
@@ -1,9 +1,26 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import { useStore } from 'vuex';
 
 const store = useStore();
 
 const imagePath = new URL(`${store.state.about.nameCard.imagePath}`, import.meta.url).href;
+
+let isBackgroundLoaded = ref(false);
+
+const imageBox = ref<HTMLDivElement | null>(null);
+const image = ref<HTMLImageElement | null>(null);
+
+function onImageLoad(){
+    isBackgroundLoaded.value = true;
+    if(imageBox.value){
+        if(image.value) image.value.style.height = imageBox.value.getBoundingClientRect().height + "px"
+    }
+};
+
+function onImageError(){
+    isBackgroundLoaded.value = false;
+};
 </script>
 
 <template>
@@ -14,8 +31,9 @@ const imagePath = new URL(`${store.state.about.nameCard.imagePath}`, import.meta
             <span v-if="store.state.about.nameCard.introduction[store.state.system.lang] && !store.state.about.nameCard.introductionHtml[store.state.system.lang]" class="introduction">{{ store.state.about.nameCard.introduction[store.state.system.lang] }}</span>
             <span v-if="store.state.about.nameCard.introduction[store.state.system.lang] && store.state.about.nameCard.introductionHtml[store.state.system.lang]" class="introduction" v-html="store.state.about.nameCard.introduction[store.state.system.lang]"></span>
         </div>
-        <div class="image-wrapper">
-            <img :src="imagePath" alt="main-user-image" class="main-image"/>
+        <div class="image-wrapper" ref="imageBox">
+            <div class="loading-spinner" v-show="!isBackgroundLoaded && imagePath"></div>
+            <img ref="image" v-if="imagePath" :src="imagePath" v-show="isBackgroundLoaded" @load="onImageLoad" @error="onImageError" alt="main-user-image" class="main-image"/>
         </div>
     </div>
 </template>
@@ -32,6 +50,9 @@ const imagePath = new URL(`${store.state.about.nameCard.imagePath}`, import.meta
     width: 100%;
     height: 70%;
     position: relative;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 }
 
 .main-image{
@@ -62,5 +83,14 @@ h4.en-name{
 
 .introduction{
     font-size: var(--Spacer-2);
+}
+
+.loading-spinner {
+  width: 30%;
+  height: 30%;
+  border: 4px solid var(--Grayscale-0);
+  border-top: 4px solid var(--Primary-50);
+  border-radius: 50%;
+  animation: spin 1s linear infinite;
 }
 </style>
