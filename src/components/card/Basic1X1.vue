a3cc68ad3da80ee0c40873fa03846fb3fa46d211
yochatapa <yochatapa@gmail.com>
2025-01-14 23:08:52 +0900

    feature: Basic 1X1 - 이미지 로딩 시, 로딩 서클 추가

--- src/components/card/Basic1X1.vue
+++ src/components/card/Basic1X1.vue
@@ -2,7 +2,7 @@
     mainText, subText, image로 이루어진 1X1 카드
 -->
 <script setup lang="ts">
-import { computed, reactive } from 'vue';
+import { computed, reactive, ref } from 'vue';
 import { useStore } from 'vuex';
 
 const store = useStore();
@@ -36,6 +36,22 @@ const mainText = computed(()=>props.cardInfo.mainText[store.state.system.lang]??
 const subText = computed(()=>props.cardInfo.subText[store.state.system.lang]??props.cardInfo.subText);
 
 const imagePath = new URL(`${props.cardInfo.imagePath}`, import.meta.url).href;
+
+let isBackgroundLoaded = ref(false);
+
+const imageBox = ref<HTMLDivElement | null>(null);
+const image = ref<HTMLImageElement | null>(null);
+
+function onImageLoad(){
+    isBackgroundLoaded.value = true;
+    if(imageBox.value){
+        if(image.value) image.value.style.height = imageBox.value.getBoundingClientRect().height + "px"
+    }
+};
+
+function onImageError(){
+    isBackgroundLoaded.value = false;
+};
 </script>
 
 <template>
@@ -45,10 +61,16 @@ const imagePath = new URL(`${props.cardInfo.imagePath}`, import.meta.url).href;
 
         <span v-if="subText && subTextHtml" class="sub-text" v-html="subText"></span>
         <span v-if="subText && !subTextHtml" class="sub-text">{{ subText }}</span>
-        <div v-if="props.cardInfo.imagePath" class="image-box" :style="{
-            backgroundImage : `url(${imagePath})`
-        }">
+
+        <div class="image-box" ref="imageBox">
+            <div class="loading-spinner" v-show="!isBackgroundLoaded && props.cardInfo.imagePath"></div>
+            <img ref="image" v-if="props.cardInfo.imagePath" :src="imagePath" :alt="mainText" v-show="isBackgroundLoaded" @load="onImageLoad" @error="onImageError"></img>
         </div>
+        
+        <!--  v-if="props.cardInfo.imagePath" class="image-box" :style="{
+            backgroundImage : `url(${imagePath})`
+        }"> 
+        </div> -->
     </div>
 </template>
 
@@ -77,8 +99,26 @@ const imagePath = new URL(`${props.cardInfo.imagePath}`, import.meta.url).href;
 .basic-1X1 .image-box {
     width: 100%;
     flex : 1;
-    background-size: contain;
-    background-position: bottom;
-    background-repeat: no-repeat;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.loading-spinner {
+  width: 30%;
+  height: 30%;
+  border: 4px solid var(--Grayscale-0);
+  border-top: 4px solid var(--Primary-50);
+  border-radius: 50%;
+  animation: spin 1s linear infinite;
+}
+
+@keyframes spin {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
 }
 </style>
